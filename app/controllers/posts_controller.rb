213eb954626cdeab83039e6232bde338c9f13e4e
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-05 15:00:24 +0300

    add authenticate actions to post controller, add associations to post and user models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:destroy]
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @posts = Post.all
