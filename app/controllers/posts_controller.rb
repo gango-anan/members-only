a5ce3f046e54f09ff81f4f99e05cfc32a1246755
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-05 04:17:00 +0300

    complete create action and index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,35 @@
 class PostsController < ApplicationController
+  # before_action :set_micropost, only: [:index]
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @posts = Post.all
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
   end
   
 end
