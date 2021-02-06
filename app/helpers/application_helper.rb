e62a0d5fb6316a2a4d05a8a9b2cd486416fcb227
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-06 17:24:06 +0300

    remove logic from posts/index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def logged_in_user(user)
+    user.email.split('@').first.capitalize
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,8 @@
 module PostsHelper
   def display_author(post)
     if user_signed_in?
-      "Posted by: #{ post.user_email.split('@').first.capitalize }"
+      user_mail = User.find_by(id: post.user_id).email
+      user_mail.split('@').first.capitalize
     end
   end
 end
