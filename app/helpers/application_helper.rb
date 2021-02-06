8f0a4052b1bc77a368dbb6932358979045537ade
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-07 00:15:11 +0300

    complete removing logic from navigation view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,23 +3,11 @@ module ApplicationHelper
     user.email.split('@').first.capitalize
   end
 
-  def links_to_display(disp_link)
-    if user_signed_in?
-      case disp_link
-      when 'Create New Post'
-        'Create New Post'
-      when 'signed_in_user'
-        "Logged in as: #{current_user.email.split('@').first.capitalize}"
-      when 'Sign Out'
-        'Sign Out'
-      end
+  def links_to_display(login_status)
+    if login_status
+      render 'layouts/user_logged_in'
     else
-      case disp_link
-      when 'Sign Up'
-        'Sign Up'
-      when 'Sign In'
-        'Sign In'
-      end
+      render 'layouts/user_not_logged_in'
     end
   end
 end
