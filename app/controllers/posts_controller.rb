77369a9412c3b9475225308982a758951bec9a46
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-06 05:00:01 +0300

    refactor code to remove unused code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  # before_action :set_post, only: [:destroy]
   before_action :authenticate_user!, except: [:index]
 
   def index
@@ -22,19 +21,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @post.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to root_path, notice: "Post was successfully deleted." }
-  #   end
-  # end
-
   private
-
-  # def set_post
-  #   @post = Post.find(params[:id])
-  # end
-
   def post_params
     params.require(:post).permit(:content, :user_id, :user_email)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
 end
