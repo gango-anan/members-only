126eeeb4edb04195693907ce8450e463166ed35e
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-06 18:57:25 +0300

    add links_to_display helper and remove logic from _navigation view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def logged_in_user(user)
     user.email.split('@').first.capitalize
   end
+
+  def links_to_display(disp_link)
+    if user_signed_in?
+      case disp_link
+      when 'Create New Post'
+        'Create New Post'
+      when "current_user.email.split('@').first.capitalize"
+        current_user.email.split('@').first.capitalize
+      when 'Sign Out'
+        'Sign Out'
+      end
+    else
+      case disp_link
+      when 'Sign Up'
+        'Sign Up'
+      when 'Sign In'
+        'Sign In'
+      end
+    end
+  end
 end
