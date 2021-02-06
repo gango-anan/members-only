595e0c15286689bc06d3ed4422a25ce4a8cd2861
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-06 05:21:56 +0300

    refactor code to fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ class PostsController < ApplicationController
   end
 
   private
+
   def post_params
     params.require(:post).permit(:content, :user_id, :user_email)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: %i[new create index]
 end
